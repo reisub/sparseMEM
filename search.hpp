067aa74cefe3fd54cd01c452e40497251e33191b
Josip Domsic <josip.domsic@gmail.com>
2015-01-05 21:27:33 +0100

    added binary search for left interval value (BsearchR). Bug: forgot braces.

--- search.hpp
+++ search.hpp
@@ -5,15 +5,15 @@
 * Searches input string (S) for suffixes having "comparing_character" at "query_offset",
 * using suffix array (SA).
 */
-long bsearchL (char comparing_character, long query_offset, long start_interval, long end_interval){
+long binary_search_left (char comparing_character, long query_offset, long start_interval, long end_interval){
 	if (comparing_character == S[SA[start_interval] + query_offset]){
-		return l;
+		return start_interval;
 	}
 	
 	long middle;
 	while ((end_interval - start_interval) > 1){
 		middle = (end_interval + start_interval) / 2;	
-		if (comparing_character < S[SA[end_interval] + query_offset]{
+		if (comparing_character <= S[SA[end_interval] + query_offset]){
 			end_interval = middle;		
 		} else {
 			start_interval = middle;
@@ -22,5 +22,24 @@ long bsearchL (char comparing_character, long query_offset, long start_interval,
 	return end_interval;
 }
 
-
-
+/**
+* Returns right bound of interval. 
+* Searches input string (S) for suffixes having "comparing_character" at "query_offset",
+* using suffix array (SA).
+*/
+long binary_search_left (char comparing_character, long query_offset, long start_interval, long end_interval){
+	if (comparing_character == S[SA[start_interval] + query_offset]){
+		return end_interval;
+	}
+	
+	long middle;
+	while ((end_interval - start_interval) > 1){
+		middle = (end_interval + start_interval) / 2;	
+		if (comparing_character < S[SA[end_interval] + query_offset]){
+			end_interval = middle;		
+		} else {
+			start_interval = middle;
+		}
+	
+	return start_interval;
+}
