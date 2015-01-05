41a971566e399cd78a86eda9369a8d35b904a5c8
Josip Domsic <josip.domsic@gmail.com>
2015-01-05 21:24:12 +0100

    added binary search for left interval value (BsearchL)

---
+++ search.hpp
@@ -0,0 +1,26 @@
+
+
+/**
+* Returns left bound of interval. 
+* Searches input string (S) for suffixes having "comparing_character" at "query_offset",
+* using suffix array (SA).
+*/
+long bsearchL (char comparing_character, long query_offset, long start_interval, long end_interval){
+	if (comparing_character == S[SA[start_interval] + query_offset]){
+		return l;
+	}
+	
+	long middle;
+	while ((end_interval - start_interval) > 1){
+		middle = (end_interval + start_interval) / 2;	
+		if (comparing_character < S[SA[end_interval] + query_offset]{
+			end_interval = middle;		
+		} else {
+			start_interval = middle;
+		}
+	
+	return end_interval;
+}
+
+
+
