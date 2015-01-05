aded8563e516366ee319e0f04ba4bc3ea425b311
Josip Domsic <josip.domsic@gmail.com>
2015-01-05 21:49:43 +0100

    tested

--- search.hpp
+++ search.hpp
@@ -1,4 +1,7 @@
+using namespace std;
 
+extern string S;
+extern long SA[];
 
 /**
 * Returns left bound of interval. 
@@ -13,12 +16,13 @@ long binary_search_left (char comparing_character, long query_offset, long start
 	long middle;
 	while ((end_interval - start_interval) > 1){
 		middle = (end_interval + start_interval) / 2;	
-		if (comparing_character <= S[SA[end_interval] + query_offset]){
+		if (comparing_character <= S[SA[middle] + query_offset]){
 			end_interval = middle;		
 		} else {
 			start_interval = middle;
 		}
-	
+	}
+
 	return end_interval;
 }
 
@@ -27,19 +31,20 @@ long binary_search_left (char comparing_character, long query_offset, long start
 * Searches input string (S) for suffixes having "comparing_character" at "query_offset",
 * using suffix array (SA).
 */
-long binary_search_left (char comparing_character, long query_offset, long start_interval, long end_interval){
-	if (comparing_character == S[SA[start_interval] + query_offset]){
+long binary_search_right (char comparing_character, long query_offset, long start_interval, long end_interval){
+	if (comparing_character == S[SA[end_interval] + query_offset]){
 		return end_interval;
 	}
 	
 	long middle;
 	while ((end_interval - start_interval) > 1){
 		middle = (end_interval + start_interval) / 2;	
-		if (comparing_character < S[SA[end_interval] + query_offset]){
+		if (comparing_character < S[SA[middle] + query_offset]){
 			end_interval = middle;		
 		} else {
 			start_interval = middle;
 		}
-	
+	}
+
 	return start_interval;
 }
