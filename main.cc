af1d9dea5c8031336feade7cd8f4b365ef934eb1
Josip Domsic <josip.domsic@gmail.com>
2015-01-11 23:23:00 +0100

    test

--- main.cc
+++ main.cc
@@ -143,7 +143,7 @@ int main(int argc, char *argv[]) {
   cout << endl;
   // Generate ISA A
   for(int i = 0; i < j; i++) {
-		sparseISA[sparseSA[i]] = i;
+		sparseISA[sparseSA[i] % j] = i;
   }
 cout << endl;
   for(int i = 0; i < j; i++) {
