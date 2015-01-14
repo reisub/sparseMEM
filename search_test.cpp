3ba2f0354a11334da267289b85c9f880812a6249
Josip Domsic <josip.domsic@gmail.com>
2015-01-14 13:19:25 +0100

    preatty code (3) :)

--- search_test.cpp
+++ search_test.cpp
@@ -5,9 +5,8 @@
 
 using namespace std;
 
-
-
 int main(){
+  // Regual suffix array
 	int SA[] = {11, 10, 7, 4, 1, 0, 9, 8, 6, 3, 5, 2};
 	string S = "mississippi$";
 	int LCP[] = {-1, 0, 1, 1, 4, 0, 0, 1, 0, 2, 1, 3};
@@ -25,8 +24,6 @@ int main(){
 	cout << "Test passed. (topdown search)" << endl;
 
 	interval_t empty, interval;
-
-
 	c = 's';
 	ret_val = topdown(c, {1, 3, 4}, S, SA);
 	assert (ret_val.depth == 2);
@@ -49,8 +46,6 @@ int main(){
 	assert (ret_val.start == 3);
 	assert (ret_val.end == 3);
 	cout << "Test passed. (topdown search)" << endl;
-	
-
 
 	// issiPpi
 	string stringg("issippi$");
@@ -60,38 +55,33 @@ int main(){
 	assert (ret_val.end == 3);
 	cout << "Test passed. (topdown search)" << endl;
 
-
-	//////////////////////////
-	// array tests:
+	// Array tests
 	cout << "Full MEM test (1), suffix array" << endl;
 	string query_string ("issip");
 	// K == 1 (step), N == size of S (11), L = min match to print (npr. 2) 
 	MEM(0, S, ISA, LCP, SA, query_string, 1, 11, 2);
 	
-	
+	// Sparse 
 	cout << "Full MEM test (2), sparse suffix array" << endl;
-	////// Sparse 
 	int sparse_SA[] = {10, 4, 0, 8, 6, 2};
 	string string_S = "mississippi$";
 	int sparse_ISA[] = {2, 5, 1, 4, 3, 0};
 	int sparse_LCP[] = {-1, 1, 0, 0, 0, 1};
 	int K = 2;
 	int L = 3;
-	
 	MEM(0, string_S, sparse_ISA, sparse_LCP, sparse_SA, query_string, K, string_S.length(), L);
 	
+	// Sparse 2
 	cout << "Full MEM test (3), sparse suffix array" << endl;
 	int SA_a[]  =  {70,45,15,40,10,65,35,5,60,30,0,55,25,50,20};
   int ISA_a []= { 10, 7, 4, 2,14,12,9,6,3,1,13,11,8,5,0};
   int LCP_a []= {  -1 ,0  ,25   ,1  ,30   ,0   ,5  ,35   ,2  ,10  ,40   ,1  ,15   ,0  ,20 };
 	query_string = "CTAACCCTAACCCTAACCCTAACCCTAACCCTAA";
 	S = "CCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTA";
-	K = 5; L = 30;
-	
-	
+	K = 5; L = 30;	
 	MEM(0, S, ISA_a, LCP_a, SA_a, query_string, K, string_S.length(), L );
-	return 0;
 	
+return 0;
 }
 
 
